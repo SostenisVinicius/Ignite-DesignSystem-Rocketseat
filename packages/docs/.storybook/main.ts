cc1ce58b3015291b1d5105a0595da9d42e2f1e14
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-24 16:45:09 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/Ignite-DesignSystem-Rocketseat/";
+    }
+    return config;
+  },
 };
 export default config;
